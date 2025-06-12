68ef0aaf5127428349122329adba2c34b8931d03
Jacob Koziej <jacobkoziej@gmail.com>
2025-06-12 23:59:12 +0200

    feat: add Nix flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+# SPDX-License-Identifier: MPL-2.0
+
+{
+  description = "Making the Jump to Advanced Embedded Programming";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) lib;
+
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC (
+          let
+            pre-commit-bin = "${lib.getBin pkgs.pre-commit}/bin/pre-commit";
+
+          in
+          {
+            packages = with pkgs; [
+              typstyle
+              commitlint-rs
+              mdformat
+              pre-commit
+              shfmt
+              statix
+              toml-sort
+              treefmt
+              yamlfmt
+              yamllint
+            ];
+
+            shellHook = ''
+              ${pre-commit-bin} install --allow-missing-config > /dev/null
+            '';
+          }
+        );
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
